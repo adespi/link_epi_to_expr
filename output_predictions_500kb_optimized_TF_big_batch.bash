04504e7f3c79bd72db715bd97b850cd4b110abc2
Antoine <adespina@mit.edu>
2019-03-29 16:59:54 -0400

    explain code, add comments

--- output_predictions_500kb_optimized_TF_big_batch.bash
+++ output_predictions_500kb_optimized_TF_big_batch.bash
@@ -1,22 +1,22 @@
 #!/bin/bash
 #avant de lancer le script : source activate kipoi-DeepSEA__predict
 overall_start=`date`
-#cd ~/link_epi_to_expr/
+#to lauch the script from any directory
+cd `dirname $0`
+#import configuration settings, not synced with git, independant on every computer
 . ./config
 
-#less nbr_snp_per_gene |sort -nr|less
-#TO EXTRACT FA SEQ:
+#loop to go through all the TFs in the list
 for TF in `tac $list_of_genes |sed '$d'|cut -f1`;do
    echo -ne "Analysing gene "`grep "$TF" $list_of_genes |cut -f6`"   "`date`"\t\t\t\t\r"
    read -r chromosome gene <<<$(less ../data/geuvadis_expression/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt.gz |cut -f1-5|grep $TF|cut -f3-4)
-#   read -r chromosome gene <<<$(less ../data/geuvadis_expression/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt.gz |cut -f1-5|grep $TF|cut -f3-4)
    gene_name=`grep "$TF" $list_of_genes |cut -f6`
    if [ "$chromosome" = '' ] || [ -e 'correlations_para/correlations_'$chromosome'_'$gene'_'$gene_name'.csv.gz' ] || [ -e 'correlations_para/correlations_'$chromosome'_'$gene'_'$gene_name'.csv' ] || [ -e 'correlations_done/correlations_'$chromosome'_'$gene'_'$gene_name'.csv.gz' ] || [ -e 'correlations_done/correlations_'$chromosome'_'$gene'_'$gene_name'.csv' ]
    then
       continue
    fi
-   #for gene in `less nbr_snp_per_gene_500kb |sort -nr|sed "s/^ *//"|cut -f2 -d ' '|head -n 7`;do
-   echo "gene $gene_name chr $chromosome position $gene, window size = $window_size, window step = $window_step "`date`
+   echo "Computing for gene $gene_name chr $chromosome position $gene, window size = $window_size, window step = $window_step "`date`
+   #new shell make in run in parallel and to filter the stderr in output screen
    echo $(for DIRECTORY in temp/ temp/`echo $chromosome'_'$gene`/ temp/`echo $chromosome'_'$gene`/intervals/ temp/`echo $chromosome'_'$gene`/fa_output/ temp/`echo $chromosome'_'$gene`/expression/ ;do
       if [ ! -d "$DIRECTORY" ]; then
          mkdir "$DIRECTORY";
@@ -25,18 +25,17 @@ for TF in `tac $list_of_genes |sed '$d'|cut -f1`;do
       fi
    done
 
+   #output fa for every patient (500kb around the interesting position)
    echo -ne "outputing fa...           \r"
    start=`date`
    for patient in `more list_commun_patients`;do
       samtools faidx ../data/hs37d5.fa.gz $chromosome:`echo $gene $window_size |awk '{print $1<$2/2 ? 1 : $1-$2/2}'`-`echo $(($gene + $window_size/2 +1000))` | bcftools consensus -i 'type="snp"' -s $patient ../data/genome_seq/ALL.chr$chromosome.phase3_shapeit2_mvncall_integrated_v*.20130502.genotypes.vcf.gz 2>/dev/null | sed "s/^>\(.*\)/>$patient:\1/" | bgzip > temp/`echo $chromosome'_'$gene`/fa_output/out`echo $chromosome'_'$gene'_'$patient`.fa.gz 2>/dev/null
    done
    fa_out_end=`date`
-   echo -ne "converting fa to kipoi input...           \r"
+
+
+   #create intervals files for every patient (needed for kipoi prediction)
    seq_arg=`echo 1 $window_step $window_size`
-   #for file in temp/`echo $chromosome'_'$gene`/fa_output/out`echo $chromosome'_'$gene'_'`*.fa;do
-   #   samtools faidx $file
-   #done
-   
 
    for file in temp/`echo $chromosome'_'$gene`/fa_output/out`echo $chromosome'_'$gene'_'`*.fa.gz;do
       name_seq=`bgzip -cd $file|head -n 1| tr -d ">"`
@@ -45,10 +44,16 @@ for TF in `tac $list_of_genes |sed '$d'|cut -f1`;do
       done > "temp/`echo $chromosome'_'$gene`/intervals/`basename $file .gz`"
    done
    create_intervals=`date`
+
+   #fetch expression for the gene and put it into a file ready for python script
    less ../data/geuvadis_expression/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt.gz |grep -e '^T' -e $'\t'$chromosome$'\t'$gene$'\t'>temp/`echo $chromosome'_'$gene`/expression/`echo $chromosome'_'$gene`.tsv
+
+
    if [ ! -d "correlations_para/" ]; then
       mkdir "correlations_para/";
    fi
+
+   #predict epigenome marks using a python script, if on eor more GPUs are avaivable, we use the GPU with the most RAM available
    echo -ne "predicting epigenome...           \r"
    if [ `command -v nvidia-smi` ]; then
       if [ `nvidia-smi -i 0 --query-gpu=utilization.memory --format=csv,noheader,nounits` -lt `nvidia-smi -i 1 --query-gpu=utilization.memory --format=csv,noheader,nounits` ];
@@ -64,6 +69,8 @@ for TF in `tac $list_of_genes |sed '$d'|cut -f1`;do
    #rm -r temp/`echo $chromosome'_'$gene`/
    echo -e "started at "$start",\nfa_output_end at "$fa_out_end",\ncreate_intervals end at "$create_intervals",\npython ended at "$python_end>correlations_para/time_for_`echo $chromosome'_'$gene'_'$gene_name`.txt) > /dev/null 2>&1 &
    sleep $wait_time_between_two_batches
+
+   #pause the script in order to have only $nbr_para_tasks jobs running at the same time
    while [ $(($nbr_para_tasks-1)) -lt `jobs -p | grep "^[0-9]"| wc -l` ]
    do
       sleep 60
