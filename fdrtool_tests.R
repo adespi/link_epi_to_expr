cff839d264047e74ab8bbf1eb224175502491b0a
Antoine <adespina@mit.edu>
2019-04-02 18:26:43 -0400

    try EBM to filter intersting locations

--- fdrtool_tests.R
+++ fdrtool_tests.R
@@ -9,34 +9,56 @@ row.names(csv)<-paste(cbind(csv[1],seq(919))$X,cbind(csv[1],seq(919))$`seq(919)`
 csv[1]<- list(NULL)
 csv[is.na(csv)] <- 0
 
-p<-0.01
+p<-0.05
+qvals=data.frame(matrix(ncol=5000,nrow=919))
+for (x in 1:919) {
+  fdr.out = fdrtool(as.vector(t(csv[x,])), statistic = "correlation",plot = F,verbose = F)
+  qvals[x,]=fdr.out$qval
+  print(x)
+  #plot(fdr.out$qval<0.05)#,ylim=c(0, 0.05))
+  #title(main = paste(gene,"q-values for one mark :",x))
+}
+write.csv(qvals, file = "qvals.tsv",sep="\t")
+plot(apply(qvals,2,min))
+which.max(apply(qvals<0.05, 2,sum))
+cols2 <- colorRampPalette(c("blue","white"))(256)
+image(as.matrix(t(qvals)),  col = cols2, zlim=c(0, 0.05))
+title(main="image of the qvalues",
+      xlab="posiyions", ylab="histone marks") 
+sum(qvals[714,]<0.05)
+which.max(apply(qvals<0.05, 0,sum))
 
-fdr.out = fdrtool(as.vector(t(csv[1,])), statistic = "correlation")
-plot(fdr.out$pval)#,ylim=c(0, 0.05))
-title(main = paste(gene,"p-values for one mark"))
-plot(as.numeric(csv[1,]))
-a<-which(fdr.out$pval<p,TRUE)
-b<-as.numeric(csv[1,])[fdr.out$pval<p]
-points(a,b,pch=19,col='red')
+fdr.out = fdrtool(as.vector(t(csv[3,])), statistic = "correlation",plot = F)
+'plot(fdr.out$pval)#,ylim=c(0, 0.05))
+title(main = paste(gene,"p-values for one mark"))'
+plot(fdr.out$qval)#,ylim=c(0, 0.05))
+title(main = paste(gene,"q-values for one mark"))
+'plot(as.numeric(csv[1,]))
+a<-which(fdr.out$qval<p,TRUE)
+b<-as.numeric(csv[1,])[fdr.out$qval<p]
+points(a,b,pch=19,col="red")
 title(main = paste(gene," corr coef for one mark (",length(a)," points with p<",p,")",sep=""))
-
+'
 'csv2<-apply((csv),2,max)
 plot(csv2)
 fdr.out = fdrtool(csv2, statistic = "correlation")
-plot(fdr.out$pval)'
+plot(fdr.out$pval)
 
 csv2<-apply(abs(csv),2,max)
 fdr.out = fdrtool(csv2, statistic = "correlation")
 plot(fdr.out$pval)
 title(main = paste(gene,"p-values for max(mark)"))
+plot(fdr.out$qval)
+title(main = paste(gene,"q-values for max(mark)"))
 plot(csv2)
-a<-which(fdr.out$pval<p,TRUE)
-b<-csv2[fdr.out$pval<p]
-points(a,b,pch=19,col='red')
+a<-which(fdr.out$qval<p,TRUE)
+b<-csv2[fdr.out$qval<p]
+points(a,b,pch=19,col="red")
 title(main = paste(gene," corr coef for max(mark) (",length(a)," points with p<",p,")",sep=""))
 
 
-'#to keep max(csv) but with negative values too
+
+#to keep max(csv) but with negative values too
 a<-apply(abs(csv),2,which.max)
 x<-1
 for (i in a){
@@ -46,7 +68,7 @@ for (i in a){
 a
 plot(a)
 fdr.out = fdrtool(a, statistic = "correlation")
-plot(fdr.out$pval)'
+plot(fdr.out$qval)
 
 
-#plot(fdr.out$pval<0.00001)
+#plot(fdr.out$pval<0.00001)'
