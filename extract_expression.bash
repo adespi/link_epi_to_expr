e30b55608f1507048dc60d3d63ec6b7966717174
antoine <adespina@mit.edu>
2019-04-24 11:42:29 -0400

    set for first time user

--- extract_expression.bash
+++ extract_expression.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm expression_temp.tsv
-for list_of_genes in "some_genes_32_Tissue-specific_regulatory_networks_FANTOM5-v1" "some_genes" "all_genes";do #"all_genes"
+for list_of_genes in "some_genes";do #"some_genes_32_Tissue-specific_regulatory_networks_FANTOM5-v1" "all_genes"
     for TF in `tac "gene_info_"$list_of_genes".txt" |sed '$d'|cut -f1`;do
         read -r chromosome gene <<<$(less ../data/geuvadis_expression/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt.gz |cut -f1-5|grep $TF|cut -f3-4)
         less ../data/geuvadis_expression/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt.gz |grep -e $'\t'$chromosome$'\t'$gene$'\t'>>expression_temp.tsv
